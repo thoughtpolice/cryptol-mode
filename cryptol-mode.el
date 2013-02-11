8f9d3addb8baa9e1854c7e6fb0382130cb9b94a2
Austin Seipp <aseipp@pobox.com>
2013-02-11 04:53:20 -0600

    Wibbles to Commentary.
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- cryptol-mode.el
+++ cryptol-mode.el
@@ -38,7 +38,10 @@
 ;; This package provides a major mode for editing, compiling and
 ;; running Cryptol code.
 ;;
-;; For more information, see http://corp.galois.com/cryptol/
+;; For more information about Cryptol, check out the homepage and
+;; documentation: http://corp.galois.com/cryptol/
+;;
+;; For usage info, release notes and bugs, check the homepage.
 
 ;;; TODO:
 
