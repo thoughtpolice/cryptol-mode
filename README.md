21aeed735f890e3d5dd406f4aa4b57b56d31fcfa
Austin Seipp <aseipp@pobox.com>
2013-02-11 00:19:55 -0600

    Initial commit.
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

---
+++ README.md
@@ -0,0 +1,46 @@
+# Cryptol Major Mode for Emacs
+
+This package provides an emacs major mode for [Cryptol][], a DSL for
+writing cryptographic code.
+
+[Cryptol]: http://corp.galois.com/cryptol/
+
+# Usage
+
+Put `cryptol-mode.el` somewhere on your `load-path` and then just add:
+
+```lisp
+(require 'cryptol-mode)
+```
+
+to your `.emacs`.
+
+Hopefully, this will one day be available on Marmalade for easy installation.
+
+# Join in
+
+Be sure to read the [contributing guidelines][contribute]. File bugs
+in the GitHub [issue tracker][].
+
+Master [git repository][gh]:
+
+* `git clone https://github.com/thoughtpolice/cryptol-mode.git`
+
+There's also a [BitBucket mirror][bb]:
+
+* `git clone https://bitbucket.org/thoughtpolice/cryptol-mode.git`
+
+# Authors
+
+See [AUTHORS.txt](https://raw.github.com/thoughtpolice/cryptol-mode/master/AUTHORS.txt).
+
+# License
+
+MIT. See
+[LICENSE.txt](https://raw.github.com/thoughtpolice/cryptol-mode/master/LICENSE.txt)
+for terms of copyright and redistribution.
+
+[contribute]: https://github.com/thoughtpolice/cryptol-mode/blob/master/CONTRIBUTING.md
+[issue tracker]: http://github.com/thoughtpolice/cryptol-mode/issues
+[gh]: http://github.com/thoughtpolice/cryptol-mode
+[bb]: http://bitbucket.org/thoughtpolice/cryptol-mode
---
+++ cryptol-mode.el
@@ -0,0 +1,114 @@
+;; Copyright (c) 2013 Austin Seipp. All rights reserved.
+;; 
+;; Permission is hereby granted, free of charge, to any person obtaining
+;; a copy of this software and associated documentation files (the
+;; "Software"), to deal in the Software without restriction, including
+;; without limitation the rights to use, copy, modify, merge, publish,
+;; distribute, sublicense, and/or sell copies of the Software, and to
+;; permit persons to whom the Software is furnished to do so, subject to
+;; the following conditions:
+;; 
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;; 
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+;; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+;; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+;; 
+;; [ MIT license: http://www.opensource.org/licenses/MIT ]
+
+;; Version:   0.0.0.0-DEV
+;; Keywords:  Cryptol Emacs Major-mode
+;; Author:    Austin Seipp <aseipp [@at] pobox [dot] com>
+;; URL:       http://github.com/thoughtpolice/cryptol-mode
+
+;; TODO:
+;;  - Everything
+
+;;; -- Customization variables -------------------------------------------------
+
+(defconst cryptol-mode-version "0.0.0.0-DEV"
+  "The version of `cryptol-mode'.")
+
+(defgroup cryptol nil
+  "A Cryptol major mode."
+  :group 'languages)
+
+(defcustom cryptol-tab-width tab-width
+  "The tab width to use when indenting."
+  :type  'integer
+  :group 'cryptol)
+
+(defcustom cryptol-command "cryptol"
+  "The Cryptol command to use for evaluating code."
+  :type  'string
+  :group 'cryptol)
+
+(defcustom cryptol-args-repl '("")
+  "The arguments to pass to `cryptol-command' to start a REPL."
+  :type  'list
+  :group 'cryptol)
+
+(defcustom cryptol-args-compile '("-b")
+  "The arguments to pass to `cryptol-command' to compile a file."
+  :type  'list
+  :group 'cryptol)
+
+(defcustom cryptol-compiled-buffer-name "*cryptol-compiled*"
+  "The name of the scratch buffer for compiled Cryptol."
+  :type  'string
+  :group 'cryptol)
+
+(defcustom cryptol-mode-hook nil
+  "Hook called by `cryptol-mode'. Examples:
+
+  TODO FIXME"
+  :type  'hook
+  :group 'cryptol)
+
+(defvar cryptol-mode-map
+  nil
+  "Keymap for `cryptol-mode'.")
+
+;;; -- Commands ----------------------------------------------------------------
+
+
+;;; -- Language Syntax ---------------------------------------------------------
+
+(defvar cryptol-string-regexp "\"\\.\\*\\?")
+
+(defvar cryptol-symbols-regexp
+  (regexp-opt '( "[" "]" ";" ":" "," "{" "}" "=>" "->" "@") 'words))
+
+(defvar cryptol-keywords-regexp
+  (regexp-opt '("where" "let" "if" "else" "then") 'words))
+
+(defvar cryptol-font-lock-defaults
+  `((,cryptol-string-regexp   . font-lock-string-face)
+    (,cryptol-symbols-regexp  . font-lock-builtin-face)
+    (,cryptol-keywords-regexp . font-lock-keyword-face)
+    ))
+
+;;; -- Mode entry --------------------------------------------------------------
+
+(define-derived-mode cryptol-mode fundamental-mode "Cryptol"
+  "Major mode for editing Cryptol files"
+  
+  ;; Syntax highlighting
+  (setq font-lock-defaults '((cryptol-font-lock-defaults)))
+  ;; Indentation, no tabs
+  (set (make-local-variable 'tab-width) cryptol-tab-width)
+  (setq indent-tabs-mode nil))
+
+; El fin.
+(provide 'cryptol-mode)
+
+;;; -- Autoloading -------------------------------------------------------------
+
+(add-to-list 'auto-mode-alist '("\\.cry$"  . cryptol-mode))
+(add-to-list 'auto-mode-alist '("\\.lcry$" . cryptol-mode))
+(add-to-list 'auto-mode-alist '("\\.scr$"  . cryptol-mode))
