15a94d35a9ff4c3a6a043b6856c2bf4361b1c82d
Austin Seipp <aseipp@pobox.com>
2013-02-11 21:49:46 -0600

    Add 'instantiate' keyword.
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- cryptol-mode.el
+++ cryptol-mode.el
@@ -126,7 +126,7 @@
   (regexp-opt '( "Bit" "inf" "fin")))
 
 (defvar cryptol-keywords-regexp
-  (regexp-opt '("module" "theorem" "where" "include"
+  (regexp-opt '("module" "theorem" "where" "include" "instantiate"
 		"let" "if" "else" "then" "type") 'words))
 
 (defvar cryptol-theorem-regexp
